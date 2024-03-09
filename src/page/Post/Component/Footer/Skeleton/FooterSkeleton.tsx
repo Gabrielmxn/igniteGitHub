230929f3928f3e539606b6bb06a5336e765fb820
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-03-09 12:01:46 -0300

    :lipstick: feat: add margin top in skeleton

--- src/page/Post/Component/Footer/Skeleton/FooterSkeleton.tsx
+++ src/page/Post/Component/Footer/Skeleton/FooterSkeleton.tsx
@@ -6,8 +6,8 @@ export function FooterSkeleton(){
   return(
     <Footer>
       <Skeleton width="100px" height="20px" margintop="1rem"/>
-      <Skeleton width="100px" height="20px"/>
-      <Skeleton width="100px" height="20px"/>
+      <Skeleton width="100px" height="20px" margintop="1rem"/>
+      <Skeleton width="100px" height="20px" margintop="1rem"/>
     </Footer> 
   )
 }
